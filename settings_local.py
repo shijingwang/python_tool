329821ad543658683215dc37e5a6fd5278ca71c4
kulen <kulen.shijingwang@gmail.com>
2014-07-22 11:28:46 +0800

    merger config

--- settings_local.py
+++ settings_local.py
@@ -5,4 +5,17 @@ MYSQL_MOLBASE = {"host": "127.0.0.1",
                "user": "root",
                "password": ""
                }
-
+MYSQL_MOLBASE = {"host": "192.168.0.100",
+               "port": "3306",
+               "database": "molbase_local",
+               "user": "root",
+               "password": "molbase1010"
+               }
+MYSQL_SPIDER_DATA = {"host": "127.0.0.1",
+               "port": "3306",
+               "database": "spider_data",
+               "user": "root",
+               "password": "rainzgq"
+               }
+NMR_FILE_PATH = "/home/kulen/NmrMsdsETL/"
+MSDS_FILE_PATH = "/home/kulen/"
