595d5c6ead2cef9c7e8ab8f4133420d70256525b
kulen <kulen.shijingwang@gmail.com>
2014-08-12 10:08:26 +0800

    merger setting

--- settings_local.py
+++ settings_local.py
@@ -17,6 +17,9 @@ MYSQL_SPIDER_DATA = {"host": "127.0.0.1",
                "user": "root",
                "password": "rainzgq"
                }
-NMR_FILE_PATH = "/home/kulen/NmrMsdsETL/"
+NMR_CHEM_FILE_PATH_S = "F:/nmrpic/"
+NMR_DB_FILE_PATH_S = "/home/kulen/NmrMsdsETL/nmrdb_origin/"
+NMR_CHEM_FILE_PATH_T = "F:/nmrpic_mark/"
+NMR_DB_FILE_PATH_T = "/home/kulen/NmrMsdsETL/nmrdb_b/"
 MSDS_FILE_PATH = "/home/kulen/"
 MATCH_MOL_CMD = "/home/kulen/checkmol04/matchmol"
